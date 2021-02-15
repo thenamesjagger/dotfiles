b8b8e0048259b37a7eee809769452bf566340d38
thenamesjagger <jaggerwarren@gmail.com>
2021-02-15 18:30:40 +0000

    first commit

---
+++ polybar/launch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar control &
+polybar workspace &
+polybar close &
+polybar notify &
+polybar open &
+polybar status &
+polybar power &
+
+
+echo "Polybar launched..."
